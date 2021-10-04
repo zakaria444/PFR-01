a9771695236ed2647aea06331d35d6a06b4342ac
zakaria444 <elismailizakariae@gmail.com>
2021-10-04 20:52:56 +0100

    Update register.php
    modification user/inscrption

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -2,41 +2,41 @@
   <div class="row">
     <div class="col-md-6 mx-auto">
       <div class="card card-body bg-light mt-5">
-        <h2>Create An Account</h2>
-        <p>Please fill out this form to register with us</p>
+        <h2>CRÉEZ VOTRE COMPTE</h2>
+        <p>Veuillez remplir ce formulaire pour vous inscrire</p>
         <form action="<?php echo URLROOT; ?>/users/register" method="post">
           <div class="form-group">
-            <label for="name">Name: <sup>*</sup></label>
+            <label for="name">Nom: <sup>*</sup></label>
             <input type="text" name="name_user" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['name']; ?>">
             <span class="invalid-feedback"><?php echo $data['name_err']; ?></span>
           </div>
           <div class="form-group">
-            <label for="email">Email: <sup>*</sup></label>
+            <label for="email">Adresse e-mail: <sup>*</sup></label>
             <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
             <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
           </div>
           <div class="form-group">
-            <label for="password">Password: <sup>*</sup></label>
+            <label for="password">Mot de passe: <sup>*</sup></label>
             <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>">
             <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
           </div>
           <div class="form-group">
-            <label for="ADRESS">ADRESS : <sup>*</sup></label>
+            <label for="ADRESS">Adresse : <sup>*</sup></label>
             <input type="text" name="ADRESS" class="form-control form-control-lg <?php echo (!empty($data['ADRESS_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['ADRESS']; ?>">
             <span class="invalid-feedback"><?php  echo $data['ADRESS_err'];   ?></span>
           </div>
           <div class="form-group">
-            <label for="num">num : <sup>*</sup></label>
+            <label for="num">téléphone : <sup>*</sup></label>
             <input type="number" name="num" class="form-control form-control-lg <?php echo (!empty($data['num_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['num']; ?>">
             <span class="invalid-feedback"><?php echo $data['num_err'];   ?></span>
           </div>
 
           <div class="row">
             <div class="col">
-              <input type="submit" value="Register" class="btn btn-success btn-block">
+              <input type="submit" value="S'inscrire" class="btn btn-success btn-block">
             </div>
             <div class="col">
-              <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-light btn-block">Have an account? Login</a>
+              <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-light btn-block">Avoir un compte? Connexion</a>
             </div>
           </div>
         </form>
